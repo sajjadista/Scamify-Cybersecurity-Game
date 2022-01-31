6ca9bf76200d2e4e8baab4e12f9421092b469026
AouIssa <51041563+AouIssa@users.noreply.github.com>
2022-01-31 20:25:15 +0800

    James Scene

---
+++ Victims/JamesStart.gd
@@ -0,0 +1,20 @@
+extends TextureButton
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+#Put the path to James dialogue
+func _on_Start_pressed():
+	get_tree().change_scene("JAMES_DIALOGUE_PATH")
