3a464e21e52b3211ab201943be9c3ab90434c96e
AouIssa <51041563+AouIssa@users.noreply.github.com>
2022-01-31 19:33:17 +0800

    Delete TextureButton.gd

--- SelectScams/TextureButton.gd
+++
@@ -1,9 +0,0 @@
-extends TextureButton
-
-
-# Called when the node enters the scene tree for the first time.
-
-func _pressed():
-	Global.prevscene = get_tree().get_current_scene().get_filename()
-	get_tree().change_scene("res://Victims/victim.tscn")
-	
